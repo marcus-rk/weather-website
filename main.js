9c79ac51ad0c3c6522d2c53789b0929065f8fc63
marcus-rk <marcus.rappenborg@gmail.com>
2023-09-18 15:46:25 +0200

    changed from onclick to eventlistener to allow enter key press

--- main.js
+++ main.js
@@ -9,15 +9,30 @@ let API_key = '08d12f3f2d447c86eae6139865e577ce';
 const inputTextElement = document.querySelector(".search-input");
 const cityNameElement = document.querySelector(".city-name");
 const temperatureElement = document.querySelector(".temp");
+const searchButton = document.querySelector(".search-button");
 
-// Onclick function for button
+// Function to search for city weather data
 function searchCity(){
+    // Use the fetch API to make a GET request to the OpenWeatherMap API
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputTextElement.value}&units=metric&appid=${API_key}`)
-        .then(response => response.json())
+        .then(response => response.json()) // Parse the response as JSON
         .then((weather) => {
             cityNameElement.innerHTML = `${weather.name}`;
             temperatureElement.innerHTML = `${weather.main.temp}°C`;
+
+            // Resetting text input field to placeholder text
+            inputTextElement.value = "";
+            inputTextElement.placeholder = "Enter City Name";
         })
         .catch(error => alert('Something went wrong: ' + error));
 }
 
+// Add event listener for button click and Enter key press on the input field
+searchButton.addEventListener("click", function() {
+    searchCity();
+});
+inputTextElement.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        searchCity();
+    }
+});
