543d99d73a46a51b6cba43496c60004cc3b1f101
marcus-rk <marcus.rappenborg@gmail.com>
2023-09-18 14:11:03 +0200

    added HTML elements to Javascript

---
+++ main.js
@@ -0,0 +1,10 @@
+// makj0005
+
+// Declare const elements from HTML into Javascript with CSS querySelector
+const inputText = document.querySelector(".search-input");
+const button = document.querySelector(".search-button");
+const cityName = document.querySelector(".city-name");
+const temperature = document.querySelector(".temp");
+
+
+
